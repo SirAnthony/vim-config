0f32c39c98f7809fc9a960dc1bacf78639f0dd30
Sir Anthony <anthony@adsorbtion.org>
2014-05-24 14:19:25 +0600

    Add jedi

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/sh
+mkdir -p autoload bundle
+curl -LSo autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git submodule add https://github.com/scrooloose/nerdtree.git bundle/nerdtree
 git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
 git submodule add https://github.com/tpope/vim-surround.git bundle/surround
@@ -12,10 +14,10 @@ git submodule add https://github.com/alfredodeza/pytest.vim.git bundle/py.test
 git submodule add https://github.com/reinh/vim-makegreen bundle/makegreen
 git submodule add https://github.com/bronson/vim-trailing-whitespace.git bundle/trailing-whitespace
 git submodule add https://github.com/jeetsukumaran/vim-buffergator.git bundle/buffergator
+git submodule add https://github.com/davidhalter/jedi-vim.git bundle/jedi-vim
 
 git submodule init
 git submodule update
 git submodule foreach git submodule init
+git submodule foreach git pull origin master
 git submodule foreach git submodule update
-
-
